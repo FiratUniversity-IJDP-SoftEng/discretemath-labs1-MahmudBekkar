1c3663876466eec51e4e72e23da05a62c7d6c987
MahmudBekkar <mahmud.bekkar99@gmail.com>
2025-11-10 15:05:57 +0300

    Update ex-1.py

--- ex-1.py
+++ ex-1.py
@@ -1,1 +1,17 @@
+#Q1 Write a function that determines if a positive integer is prime.
+def is_prime(n):
+    if n <= 1:
+        return False
+    for i in range(2, int(n**0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(2))  
+print(is_prime(7))   
+print(is_prime(10)) 
+print(is_prime(2))   
+print(is_prime(66))   
+print(is_prime(33))  
+print(is_prime(20))   
 
